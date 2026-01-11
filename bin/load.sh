c8db564008336d7eacf1ff317c960a72a2cbfecb
Ian Rasbheri <ian@irasbheri.lan>
2026-01-11 16:56:12 +0000

    Script to load DE5 KSBs

---
+++ bin/load.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+ulwazi ksb K1 --add "Processes to monitor and optimise the performance of the availability, management and performance of data product."
+
+ulwazi ksb K2 --add "Methodologies for moving data from one system to another for storage and further handling."
+
+ulwazi ksb K3 --add "Data normalisation principles and the advantages they achieve in databases for data protection, redundancy, and inconsistent dependency."
+
+ulwazi ksb K4 --add "Frameworks for data quality, covering dimensions such as accuracy, completeness, consistency, timeliness, and accessibility."
+
+ulwazi ksb K5 --add "The inherent risks of data such as incomplete data, ethical data sources and how to ensure data quality."
+
+ulwazi ksb K6 --add "Software development principles for data products, including debugging, version control, and testing."
+
+ulwazi ksb K7 --add "Principles of sustainable data products and organisational responsibilities for environmental social governance."
+
+ulwazi ksb K8 --add "Deployment approaches for new data pipelines and automated processes."
+
+ulwazi ksb K9 --add "How to build a data product that complies with regulatory requirements."
+
+ulwazi ksb K10 --add "Concepts of data governance, including regulatory requirements, data privacy, security, and quality control. Legislation and its application to the safe use of data."
+
+ulwazi ksb K11 --add "Data and information security standards, ethical practices, policies and procedures relevant to data management activities such as data lineage and metadata management."
+
+ulwazi ksb K12 --add "How to cost and build a system whilst ensuring that organisational strategies for sustainable, net zero technologies are considered."
+
+ulwazi ksb K13 --add "The implications of financial, strategic and compliance regarding to security, scalability, compliance and cost of local, remote or distributed solutions."
+
+ulwazi ksb K14 --add "The uses of on-demand Cloud computing platform(s) in a public or private environment such as Amazon AWS, Google Cloud, Hadoop, IBM Cloud, Salesforce and Microsoft Azure."
+
+ulwazi ksb K15 --add "Data warehousing principles, including techniques such as star schemas, data lakes, and data marts."
+
+ulwazi ksb K16 --add "Principles of data, including open and public data, administrative data, and research data including the value of external data sources that can be used to enrich internal data. Examples of how business use direct data acquisition to support or augment business operations."
+
+ulwazi ksb K17 --add "Approaches to data integration and how combining disparate data sources delivers value to an organisation."
+
+ulwazi ksb K18 --add "How to use streaming, batching and on-demand services to move data from one location to another."
+
+ulwazi ksb K19 --add "Differences between structured, semi-structured, and unstructured data."
+
+ulwazi ksb K20 --add "Types and uses of data engineering tools and applications in own organisation."
+
+ulwazi ksb K21 --add "Policies and strategies to ensure business continuity for operations, particularly in relation to data provision."
+
+ulwazi ksb K22 --add "Technology and service management best practice including configuration, change and incident management."
+
+ulwazi ksb K23 --add "How to undertake analysis and root cause investigation."
+
+ulwazi ksb K24 --add "Processes for evaluating prototypes and taking them to implementation within a production environment."
+
+ulwazi ksb K25 --add "The lifecycle of implementing data solutions in a business, from scoping, though prototyping, development, production, and continuous improvement."
+
+ulwazi ksb K26 --add "Data development frameworks and approved organisational architectures."
+
+ulwazi ksb K27 --add "The principles of descriptive, predictive and prescriptive analytics."
+
+ulwazi ksb K28 --add "Continuous improvement including how to: capture good practice and lessons learned."
+
+ulwazi ksb K29 --add "Strategies for keeping up to date with new ways of working and technological developments in data science, data engineering and AI."
+
+ulwazi ksb K30 --add "The methods and techniques used to communicate messages to meet the needs of the audience."
+
+ulwazi ksb S1 --add "Collate, evaluate and refine user requirements to design the data product."
+
+ulwazi ksb S2 --add "Collate, evaluate and refine business requirements including cost, resourcing, and accessibility to design the data product."
+
+ulwazi ksb S3 --add "Design a data product to serve multiple needs and with scalability, efficiency, and security in mind."
+
+ulwazi ksb S4 --add "Automate data pipelines such as batch, real-time, on demand and other processes using programming languages and data integration platforms with graphical user interfaces."
+
+ulwazi ksb S5 --add "Produce and maintain technical documentation explaining the data product, that meets organisational, technical and non-technical user requirements, retaining critical information."
+
+ulwazi ksb S6 --add "Systematically clean, validate, and describe data at all stages of extract, transform, load (ETL)."
+
+ulwazi ksb S7 --add "Work with different types of data stores, such as SQL, NoSQL, and distributed file system."
+
+ulwazi ksb S8 --add "Identify and troubleshoot issues with data processing pipelines."
+
+ulwazi ksb S9 --add "Query and manipulate data using tools and programming such as SQL and Python. Manage database access, and implement automated validation checks."
+
+ulwazi ksb S10 --add "Communicate downtime and issues with database access to stakeholders to mitigate the operational impact of unforeseen issues."
+
+ulwazi ksb S11 --add "Evaluate opportunities to extract value from existing data products through further development, considering costs, environmental impact and potential operational benefits."
+
+ulwazi ksb S12 --add "Maintain a working knowledge of data use cases within organisations."
+
+ulwazi ksb S13 --add "Use data systems securely to meet requirements and in line with organisational procedures and legislation."
+
+ulwazi ksb S14 --add "Identify new tools and technologies and recommend potential opportunities for use in own department or organisation."
+
+ulwazi ksb S15 --add "Optimise data ingestion processes by making use of appropriate data ingestion frameworks such as batch, streaming and on-demand."
+
+ulwazi ksb S16 --add "Develop algorithms and processes to extract structured data from unstructured sources."
+
+ulwazi ksb S17 --add "Apply and advocate for software development best practice when working with other data professionals throughout the business. Contribute to standards and ways of working that support software development principles."
+
+ulwazi ksb S18 --add "Develop simple forecasts and monitoring tools to anticipate or respond immediately to outages and incidents."
+
+ulwazi ksb S19 --add "Identify and escalate risks with suggested mitigation/resolutions as appropriate."
+
+ulwazi ksb S20 --add "Investigate and respond to incidents, identifying the root cause and resolution with internal and external stakeholders."
+
+ulwazi ksb S21 --add "Identify and remediate technical debt, assess for updates and obsolescence as part of continuous improvement."
+
+ulwazi ksb S22 --add "Develop, maintain collaborative relationships using adaptive business methodology with stakeholders such as, business users, data scientists, data analysts and business intelligence teams."
+
+ulwazi ksb S23 --add "Present, communicate, and disseminate messages about the data product, tailoring the message and medium to the needs of the audience."
+
+ulwazi ksb S24 --add "Evaluate the strengths and weaknesses of prototype data products and how these integrate within an organisation’s overarching data infrastructure."
+
+ulwazi ksb S25 --add "Assess and identify gaps in existing tools and technologies in respect of implementing changes required."
+
+ulwazi ksb S26 --add "Identify data quality metrics and track them to ensure the quality, accuracy and reliability of the data product."
+
+ulwazi ksb S27 --add "Selects and apply sustainable solutions to contribute to net zero and environmental strategies across the various stages of product and service delivery."
+
+ulwazi ksb S28 --add "Horizon scanning to identify new technologies that offer increased performance of data products."
+
+ulwazi ksb S29 --add "Implement personal strategies to keep up to date with new technology and ways of working."
+
+ulwazi ksb B1 --add "Acts proactively and takes accountability adapting positively to changing work priorities, ensuring deadlines are met."
+
+ulwazi ksb B2 --add "Works collaboratively with stakeholders and colleagues, developing strong working relationships to achieve common goals. Support an inclusive culture and treat technical and non- technical colleagues and stakeholders with respect."
+
+ulwazi ksb B3 --add "Quality focus that promotes continuous improvement utilising peer review techniques, innovation and creativity to the data system development process to improve processes and address business challenges."
+
+ulwazi ksb B4 --add "Takes personal responsibility towards net zero and prioritises environmental sustainability outcomes in how they carry out the duties of their role."
+
+ulwazi ksb B5 --add "Use initiative and innovation to problem solve and trouble shoot, providing creative solutions."
+
+ulwazi ksb B6 --add "Keeps abreast of developments in emerging, contemporary and advanced technologies to optimise sustainable data products and services."
+
